1673f9a6c591dd173895892ab3fd773eeddd9a3d
JamieMaple <jamiemaple007@gmail.com>
2018-01-20 18:26:41 +0800

    + chap03

---
+++ chap03/5.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        long i1 = 0x2333E;
+        long i2 = 0771;
+
+        System.out.println(i1);
+        System.out.println(Long.toBinaryString(i1));
+        System.out.println(i2);
+        System.out.println(Long.toBinaryString(i2));
+    }
+}
