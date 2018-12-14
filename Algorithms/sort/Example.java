0389984dc8d99c71db30b8c9ee9afeec9bd71cda
Jamie Maple <jamiemaple007@gmail.com>
2018-12-14 17:31:04 +0800

    feat(sort): add shell sort

--- Algorithms/sort/Example.java
+++ Algorithms/sort/Example.java
@@ -1,11 +1,22 @@
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Example {
     public static void main(String[] args) {
-        String[] a = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
-        Selection.sort(a);
-        assert isSorted(a);
-        show(a);
+        //String[] a = { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E" };
+        //Selection.sort(a);
+        //Insertion.sort(a);
+        //assert isSorted(a);
+        //show(a);
+        String alg1 = args[0];
+        String alg2 = args[1];
+        int N = Integer.parseInt(args[2]);
+        int T = Integer.parseInt(args[3]);
+        double t1 = SortCompare.timeRandomInput(alg1, N, T);
+        double t2 = SortCompare.timeRandomInput(alg2, N, T);
+        StdOut.printf("For %d random Doubles\n  %s is", N, alg1);
+        StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
     }
 
     private static void show(Comparable[] arr) {
@@ -26,6 +37,34 @@ public class Example {
     }
 }
 
+class SortCompare {
+    public static double time(String alg, Comparable[] arr) {
+        Stopwatch timer = new Stopwatch();
+        if (alg.equals("Insertion")) {
+            Insertion.sort(arr);
+        } else if (alg.equals("Selection")) {
+            Selection.sort(arr);
+        } else if (alg.equals("Shell")) {
+            Shell.sort(arr);
+        } else {
+            return 0;
+        }
+        return timer.elapsedTime();
+    }
+
+    public static double timeRandomInput(String alg, int N, int T) {
+        double total = 0.0;
+        Double[] arr = new Double[N];
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < N; j++) {
+                arr[j] = StdRandom.uniform();
+            }
+            total += time(alg, arr);
+        }
+        return total;
+    }
+}
+
 class SortBase {
     public static void exch(Comparable[] arr, int i, int j) {
         Comparable t = arr[i];
@@ -38,6 +77,7 @@ class SortBase {
     }
 }
 
+// O(n^2), swap: n
 class Selection extends SortBase {
     public static void sort(Comparable[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
@@ -48,6 +88,43 @@ class Selection extends SortBase {
                 }
             }
             exch(arr, i, minIndex);
+        }
+    }
+}
+
+// N-1 <= O <= O(n^2/2), 0 <= swap <= O(n^2/2)
+class Insertion extends SortBase {
+    public static void sort(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less(arr[j], arr[j-1])) {
+                    exch(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+class Shell extends SortBase {
+    public static void sort(Comparable[] arr) {
+        int N = arr.length;
+        int h = 1;
+        while (h < N/3) {
+            h = 3 * h + 1;
+        }
+        while (h >= 1) {
+            for (int i = h; i < N; i++) {
+                for (int j = i; j >= h; j-= h)  {
+                    if (less(arr[j], arr[j-h])) {
+                        exch(arr, j, j-h);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            h = h / 3;
         }
     }
 }
